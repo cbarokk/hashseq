90765be73bbe006d8712ef55a83a1ba115dcb89f
cbarokk <cbarokk@gmail.com>
2016-04-28 12:28:01 +0200

    new model for Gro

--- model/LSTM_theta.lua
+++ model/LSTM_theta.lua
@@ -1,5 +1,3 @@
---require 'util.MyBatchNormalization'
-
 local LSTM_theta = {}
 function LSTM_theta.lstm()
   local rnn_size = opt.rnn_size
@@ -29,7 +27,7 @@ function LSTM_theta.lstm()
     local prev_c = inputs[L*2+1]
     -- the input to this layer
     if L == 1 then 
-      theta_x = inputs[1]
+      local theta_x = inputs[1]
       e_x = inputs[2]
       e_embedings = nn.LookupTable(num_events, embedings_size)(e_x):annotate{name='emb_e'}
       e_embedings = nn.Reshape(embedings_size)(e_embedings)
@@ -39,7 +37,8 @@ function LSTM_theta.lstm()
       
     else 
       x = outputs[(L-1)*2] 
-      --x = MyBatchNormalization(input_size_L)(x):annotate{name='bn_' .. L}
+      x = nn.BatchNormalization(input_size_L)(x):annotate{name='bn_' .. L}
+      
       if dropout > 0 then x = nn.Dropout(dropout)(x) end -- apply dropout, if any
       input_size_L = rnn_size
 
@@ -48,9 +47,11 @@ function LSTM_theta.lstm()
     
     
     local i2h = nn.Linear(input_size_L, 4 * rnn_size)(x):annotate{name='i2h_'..L}
-    --i2h = MyBatchNormalization(4 * rnn_size)(i2h):annotate{name='bn_i2h_' .. L}
+    i2h = nn.BatchNormalization(4 * rnn_size)(i2h):annotate{name='bn_i2h_' .. L}
+    
     local h2h = nn.Linear(rnn_size, 4 * rnn_size)(prev_h):annotate{name='h2h_'..L}
-    --h2h = MyBatchNormalization(4 * rnn_size)(h2h):annotate{name='bn_h2h_' .. L}
+    h2h = nn.BatchNormalization(4 * rnn_size)(h2h):annotate{name='bn_h2h_' .. L}
+    
     local all_input_sums = nn.CAddTable()({i2h, h2h})
 
     local reshaped = nn.Reshape(4, rnn_size)(all_input_sums)
@@ -74,15 +75,26 @@ function LSTM_theta.lstm()
     
   end
 
-  local layer = outputs[#outputs]
+  --local layer = outputs[#outputs]
+  local non_lin_input = nn.Linear(theta_size, embedings_size)(inputs[1]):annotate{name='non_lin_i'}
+  non_lin_input = nn.BatchNormalization(embedings_size)(non_lin_input)
+  non_lin_input = nn.Tanh()(non_lin_input)
   
-  if dropout > 0 then layer = nn.Dropout(dropout)(layer) end
+  non_lin_input = nn.Linear(embedings_size, embedings_size)(non_lin_input):annotate{name='non_lin_i'}
+  non_lin_input = nn.BatchNormalization(embedings_size)(non_lin_input)
+  non_lin_input = nn.Tanh()(non_lin_input)
   
-  local time_pred = nn.Linear(rnn_size, num_time_slots)(layer)
+  local last_h = outputs[#outputs]
+  if dropout > 0 then last_h = nn.Dropout(dropout)(last_h) end
+
+  local layer = nn.JoinTable(2)({non_lin_input, last_h}) 
+  local layer_size = embedings_size + rnn_size
+
+  local time_pred = nn.Linear(layer_size, num_time_slots)(layer)
   time_pred = nn.LogSoftMax()(time_pred)
   table.insert(outputs, time_pred)
   
-  local event_pred = nn.Linear(rnn_size, num_events)(layer)
+  local event_pred = nn.Linear(layer_size, num_events)(layer)
   event_pred = nn.LogSoftMax()(event_pred)
   table.insert(outputs, event_pred)
   
